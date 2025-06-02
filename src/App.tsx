df576016105ba261585069f5982728fd540ae8dd
Aryan Das <122392506+aryandas2911@users.noreply.github.com>
2025-06-02 16:46:39 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ export default function App() {
               </PrivateRoute>
             }
           />
-          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/" element={<Navigate to="/login\" replace />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
